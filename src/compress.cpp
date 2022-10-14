0cedcbe3ad5819f614a63a1d02dd96e25990542d
Alex Ikeda <54652078+alxkeda@users.noreply.github.com>
2022-10-14 16:41:49 -0600

    final commit

--- src/compress.cpp
+++ src/compress.cpp
@@ -9,7 +9,7 @@
 #include "../include/cf.h"
 #include "../include/encode.h"
 
-const double ENCODING = 8.0;
+// const double ENCODING = ;
 // provide the original encoding size above then compile
 
 double retrieve_cpu_time(){
